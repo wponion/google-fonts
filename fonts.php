121461f85cfe87529ee8755b3660e8972c46866d
Google WPOnion <wponion@gmail.com>
2020-05-03 00:05:59 +0000

    Google Fonts Updated

--- fonts.php
+++ fonts.php
@@ -1,5 +1,5 @@
 <?php
-// Last Updated : 2020-04-26 05:35:57:am
+// Last Updated : 2020-05-03 05:35:59:am
 if ( ! defined( "ABSPATH") ) { die; } 
 return array (
   'ABeeZee' => 
@@ -2685,6 +2685,27 @@ return array (
     '400italic' => 'Regular 400 Italic',
     '600italic' => 'Semi-Bold 600 Italic',
     '700italic' => 'Bold 700 Italic',
+  ),
+  'Jost' => 
+  array (
+    100 => 'Thin 100',
+    200 => 'Extra Light 200',
+    300 => 'Light 300',
+    400 => 'Regular 400',
+    500 => 'Medium 500',
+    600 => 'Semi-Bold 600',
+    700 => 'Bold 700',
+    800 => 'Extra Bold 800',
+    900 => 'Black 900',
+    '100italic' => 'Thin 100 Italic',
+    '200italic' => 'Extra Light 200 Italic',
+    '300italic' => 'Light 300 Italic',
+    '400italic' => 'Regular 400 Italic',
+    '500italic' => 'Medium 500 Italic',
+    '600italic' => 'Semi-Bold 600 Italic',
+    '700italic' => 'Bold 700 Italic',
+    '800italic' => 'Extra Bold 800 Italic',
+    '900italic' => 'Black 900 Italic',
   ),
   'Joti One' => 
   array (
