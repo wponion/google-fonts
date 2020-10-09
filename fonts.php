1e32942f21f8876344e8c3fa209362db61f918af
Google WPOnion <wponion@gmail.com>
2020-10-09 00:42:19 +0000

    Google Fonts Updated

--- fonts.php
+++ fonts.php
@@ -1,5 +1,5 @@
 <?php
-// Last Updated : 2020-10-08 06:05:40:am
+// Last Updated : 2020-10-09 06:12:19:am
 if ( ! defined( "ABSPATH") ) { die; } 
 return array (
   'ABeeZee' => 
@@ -1292,6 +1292,18 @@ return array (
   array (
     400 => 'Regular 400',
   ),
+  'Commissioner' => 
+  array (
+    100 => 'Thin 100',
+    200 => 'Extra Light 200',
+    300 => 'Light 300',
+    400 => 'Regular 400',
+    500 => 'Medium 500',
+    600 => 'Semi-Bold 600',
+    700 => 'Bold 700',
+    800 => 'Extra Bold 800',
+    900 => 'Black 900',
+  ),
   'Concert One' => 
   array (
     400 => 'Regular 400',
@@ -1468,8 +1480,12 @@ return array (
   'Cuprum' => 
   array (
     400 => 'Regular 400',
+    500 => 'Medium 500',
+    600 => 'Semi-Bold 600',
     700 => 'Bold 700',
     '400italic' => 'Regular 400 Italic',
+    '500italic' => 'Medium 500 Italic',
+    '600italic' => 'Semi-Bold 600 Italic',
     '700italic' => 'Bold 700 Italic',
   ),
   'Cute Font' => 
@@ -4933,6 +4949,8 @@ return array (
   array (
     300 => 'Light 300',
     400 => 'Regular 400',
+    500 => 'Medium 500',
+    600 => 'Semi-Bold 600',
     700 => 'Bold 700',
   ),
   'Redressed' => 
