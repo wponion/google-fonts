276800f2de70cba58a8bf00744c38b503777b1dc
Google WPOnion <wponion@gmail.com>
2023-07-28 01:07:58 +0000

    Google Fonts Updated

--- fonts-min.php
+++ fonts-min.php
@@ -1,4 +1,4 @@
 <?php
-// Last Updated : 2023-07-27 06:37:03:am
+// Last Updated : 2023-07-28 06:37:58:am
 if ( ! defined( "ABSPATH") ) { die; } 
 return array() ;
--- fonts.php
+++ fonts.php
@@ -1,5 +1,5 @@
 <?php
-// Last Updated : 2023-07-27 06:37:03:am
+// Last Updated : 2023-07-28 06:37:58:am
 if ( ! defined( "ABSPATH") ) { die; } 
 return array (
 ) ;
