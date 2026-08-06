224d4ff767678fabce12293fedaba791946de3cb
Google WPOnion <wponion@gmail.com>
2026-08-06 02:34:15 +0000

    Google Fonts Updated

--- fonts-min.php
+++ fonts-min.php
@@ -1,4 +1,4 @@
 <?php
-// Last Updated : 2026-08-05 08:01:47:am
+// Last Updated : 2026-08-06 08:04:15:am
 if ( ! defined( "ABSPATH") ) { die; } 
 return array() ;
--- fonts.php
+++ fonts.php
@@ -1,5 +1,5 @@
 <?php
-// Last Updated : 2026-08-05 08:01:47:am
+// Last Updated : 2026-08-06 08:04:15:am
 if ( ! defined( "ABSPATH") ) { die; } 
 return array (
 ) ;
